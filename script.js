ff617b0adeffd49f9f77451bb677450db0a18df6
fronm-4 <fronczaknatalia150@gmail.com>
2025-06-07 16:21:16 +0300

    MEGA EDIT NEW INDEX

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const forecastButton = document.querySelector(".button");
+
+  forecastButton.addEventListener("click", function () {
+    window.location.href = "index2.html";
+  });
+});
