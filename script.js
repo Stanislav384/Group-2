291a048cf201d88f03b4f6414b63efd456cf6967
fronm-4 <fronczaknatalia150@gmail.com>
2025-06-10 23:33:54 +0300

    Mega Update Add Setting and work button show less

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   const forecastButton = document.querySelector(".button");
-
-  forecastButton.addEventListener("click", function () {
-    window.location.href = "index2.html";
-  });
+  if (forecastButton) {
+    forecastButton.addEventListener("click", function () {
+      window.location.href = "index2.html";
+    });
+  }
+  const showLessButton = document.querySelector(".button2");
+  if (showLessButton) {
+    showLessButton.addEventListener("click", function () {
+      window.location.href = "index.html";
+    });
+  }
+  const settingsButton = document.querySelector(".settings-button");
+  if (settingsButton) {
+    settingsButton.addEventListener("click", function () {
+      window.location.href = "settings.html";
+    });
+  }
 });
