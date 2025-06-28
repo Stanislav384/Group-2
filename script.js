00af276b30dacf73b6b48c98c6c06a7294ed585c
fronm-4 <fronczaknatalia150@gmail.com>
2025-06-28 14:25:40 +0300

    javascript

--- script.js
+++ script.js
@@ -5,18 +5,39 @@ document.addEventListener("DOMContentLoaded", function () {
       window.location.href = "index2.html";
     });
   }
+
   const showLessButton = document.querySelector(".button2");
   if (showLessButton) {
     showLessButton.addEventListener("click", function () {
       window.location.href = "index.html";
     });
   }
+
   const settingsButton = document.querySelector(".settings-button");
   if (settingsButton) {
     settingsButton.addEventListener("click", function () {
       window.location.href = "settings.html";
     });
   }
-});
 
-document.body.style.overflow = 'hidden';
+  const forecastButton3 = document.querySelector(".button3");
+  if (forecastButton3) {
+    forecastButton3.addEventListener("click", function () {
+      window.location.href = "index2 copy.html";
+    });
+  }
+
+  const forecastButton4 = document.querySelector(".button4");
+  if (forecastButton4) {
+    forecastButton4.addEventListener("click", function () {
+      window.location.href = "index-night.html";
+    });
+  }
+
+  const settingsButton2 = document.querySelector(".settings-button2");
+  if (settingsButton2) {
+    settingsButton2.addEventListener("click", function () {
+      window.location.href = "setting-night.html";
+    });
+  }
+});
